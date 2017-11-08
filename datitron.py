21fa83e28e7115c92bdf5c6b09f071ad275e997e
gperrio <guillaume.perrio@gmail.com>
2017-11-08 13:03:53 +0100

    Update datitron.py

--- datitron.py
+++ datitron.py
@@ -12,6 +12,7 @@
 
 month2016 = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 month2017 = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+month2018 = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 def getDay(nDay, month):
     i = 1
@@ -26,9 +27,10 @@ def getDay(nDay, month):
 def main():
     yPrefix2016 = "2016"
     yPrefix2017 = "2017"
+    yPrefix2018 = "2018"
     offset2016 = 3
     offset2017 = 1
-    bBisec = 1
+    offset2018 = 0
 
     while 1:
         cMonth = month2016;
@@ -46,8 +48,14 @@ def main():
             cOffset = offset2017
             yPrefix = yPrefix2017
         pass
+		
+        if i[0] == '8':
+            cMonth = month2018
+            cOffset = offset2018
+            yPrefix = yPrefix2018
+        pass
         
-        if len(i) != 5 or i[0] != '6' and i[0] != '7' or i[4] > '5' or i[3] != '.':
+        if len(i) != 5 or i[0] != '6' and i[0] != '7' and i[0] != '8' or i[4] > '5' or i[3] != '.':
             continue
         pass
 
